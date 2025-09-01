2523fdb9402b0bdadb1bf1db19ef81e4e3b88c68
Esther Aaton <estheraaton77@gmail.com>
2025-09-01 08:31:26 -0700

    Initial commit

--- Loan_default_risk_app.py
+++ Loan_default_risk_app.py
@@ -13,16 +13,12 @@ st.title("Loan Default Risk Predictor App")
 st.write("Fill in the Borrower's Financial and Demographic Information")
 
 #User Input
-# 'referred': ['False'],'bank_account_type','bank_name_clients','employment_status_clients','age_group','payment_status','repayment_ratio'
-#'loanamount','totaldue','termdays','avg_credit_score','loan_approval_time_min','loan_counts','max_loan_amount','avg_loan_amount','longitude_gps'
-#'latitude_gps'
-
-referred = st.selectbox("Referral Status", ['True', 'False'])
-bank_account_type = st.selectbox("Bank Account Type", ['Other', 'Savings', 'Current'])
+referred = st.selectbox("Referral Status", ["True", "False"])
+bank_account_type = st.selectbox("Bank Account Type", ["Other", "Savings", "Current"])
 bank_name_clients = st.text_input("Enter your bank name", "GT Bank", max_chars=20)
-employment_status_clients = st.selectbox("Employment Status", ['Permanent', 'unknown', 'Unemployed', 'Self-Employed', 'Student', 'Retired', 'Contract'])
-age_group = st.selectbox("Age Group", ['18-25', '26-35', '36-50', '51-65', '65+'])
-payment_status = st.selectbox("Payment Status", ['on_time', 'late'])
+employment_status_clients = st.selectbox("Employment Status", ["Permanent", "unknown", "Unemployed", "Self-Employed", "Student", "Retired", "Contract"])
+age_group = st.selectbox("Age Group", ["18-25", "26-35", "36-50", "51-65", "65+"])
+payment_status = st.selectbox("Payment Status", ["on_time", "late"])
 repayment_ratio = st.number_input("Repayment Ratio", 1.0, 1.5, 1.3)
 loanamount = st.number_input("Loan Amount", 10000.0, 60000.0, 30000.0)
 totaldue = st.number_input("Total Due", 10000.0, 70000.0, 35000.0)
@@ -38,23 +34,23 @@ latitude_gps = st.number_input("Enter Latitude (optional)", value=0.0, format="%
 #Prediction Button
 if st.button("Predict the Loan Default Risk"):
   customer_info = {
-    'referred': [referred],
-    'bank_account_type': [bank_account_type],
-    'bank_name_clients': [bank_name_clients],
-    'employment_status_clients': [employment_status_clients],
-    'age_group': [age_group],
-    'payment_status': [payment_status],
-    'repayment_ratio':[repayment_ratio],
-    'loanamount':[loanamount],
-    'totaldue':[totaldue],
-    'termdays':[termdays],
-    'avg_credit_score':[avg_credit_score],
-    'loan_approval_time_min':[loan_approval_time_min],
-    'loan_counts':[loan_counts],
-    'max_loan_amount':[max_loan_amount],
-    'avg_loan_amount':[avg_loan_amount],
-    'longitude_gps':[longitude_gps],
-    'latitude_gps':[latitude_gps]
+    "Referral_status": [referred],
+    "Bank_account_type": [bank_account_type],
+    "Bank_name": [bank_name_clients],
+    "Employment_status": [employment_status_clients],
+    "Age_group": [age_group],
+    "Payment_status": [payment_status],
+    "Repayment_ratio":[repayment_ratio],
+    "Loanamount":[loanamount],
+    "Totaldue":[totaldue],
+    "Termdays":[termdays],
+    "Avg_credit_score":[avg_credit_score],
+    "Loan_approval_time_min":[loan_approval_time_min],
+    "Loan_counts":[loan_counts],
+    "Max_loan_amount":[max_loan_amount],
+    "Avg_loan_amount":[avg_loan_amount],
+    "Longitude_gps":[longitude_gps],
+    "Latitude_gps":[latitude_gps]
   }
   #Converting into a DataFrame
   input_df = pd.DataFrame(customer_info)
